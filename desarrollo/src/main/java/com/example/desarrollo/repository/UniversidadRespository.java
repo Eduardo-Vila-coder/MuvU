5ed02a876c5f62d5e1696c07ccb95719ff5182a5
Yerik Vega <yerik.vega@utec.edu.pe>
2026-09-12 12:22:14 -0500

    creando ImagenRepository, PagoPublicidadRepository y UniversidadRespository; de paso elimino los placeholder.txt

---
+++ desarrollo/src/main/java/com/example/desarrollo/repository/UniversidadRespository.java
@@ -0,0 +1,7 @@
+package com.example.desarrollo.repository;
+
+import com.example.desarrollo.model.Universidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversidadRespository extends JpaRepository<Universidad, Long> {
+}
