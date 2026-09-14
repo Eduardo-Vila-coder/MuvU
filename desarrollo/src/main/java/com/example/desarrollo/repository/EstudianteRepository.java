667b33f639bd2afae64968c4fd6eee64f40ddc63
MatCavUTEC <mathias.cavalcanti@utec.edu.pe>
2026-09-13 20:17:41 -0500

    Entidades creadas: Calificacion, Estudiante y Usuario. Entidades modificadas: Habitacion y Arrendador. Creacion de repositorys (#10)
    * Entidades creadas: Calificacion, Estudiante y Usuario.
    Entidades modificadas: Habitacion y Arrendador.
    Creacion de repositorios
    
    * Se soluciono el @allargsconstructor
    
    * fix: arreglo de repositorios y agregación de noargsconstructor

---
+++ desarrollo/src/main/java/com/example/desarrollo/repository/EstudianteRepository.java
@@ -0,0 +1,7 @@
+package com.example.desarrollo.repository;
+
+import com.example.desarrollo.model.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long>  {
+}
