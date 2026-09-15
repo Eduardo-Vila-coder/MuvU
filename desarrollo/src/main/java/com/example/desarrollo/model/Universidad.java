1c327899725ddbc5c909cede2fac53c040368511
cristhianjinchuna-dev <cristhian.jinchuna@utec.edu.pe>
2026-09-15 06:52:18 -0500

    fix: Corrige y pone id a Universidad

--- desarrollo/src/main/java/com/example/desarrollo/model/Universidad.java
+++ desarrollo/src/main/java/com/example/desarrollo/model/Universidad.java
@@ -1,9 +1,6 @@
 package com.example.desarrollo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Universidad {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String nombre;
 
     private String direccion;
