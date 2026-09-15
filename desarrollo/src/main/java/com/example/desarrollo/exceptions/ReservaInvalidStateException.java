d1727ed9043cdd11334cc00659ec88c8848cbba1
MQS144 <64620362+hfuv@users.noreply.github.com>
2026-09-15 10:55:11 -0500

    fit: creacion de una excepcion para reserva

---
+++ desarrollo/src/main/java/com/example/desarrollo/exceptions/ReservaInvalidStateException.java
@@ -0,0 +1,7 @@
+package com.example.desarrollo.exceptions;
+
+public class ReservaInvalidStateException extends RuntimeException {
+    public ReservaInvalidStateException(String message) {
+        super(message);
+    }
+}
