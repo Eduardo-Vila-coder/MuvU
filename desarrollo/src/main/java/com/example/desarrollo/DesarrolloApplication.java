93258e87e86daa51cf8c886d368af7bce873dbf4
Yerik Vega <yerik.vega@utec.edu.pe>
2026-09-23 13:53:06 -0500

    feat: modificando el pago publicidad y haciendo que el atributo de Habitacion pase a "False" despues de ciertos días

--- desarrollo/src/main/java/com/example/desarrollo/DesarrolloApplication.java
+++ desarrollo/src/main/java/com/example/desarrollo/DesarrolloApplication.java
@@ -2,8 +2,10 @@ package com.example.desarrollo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DesarrolloApplication {
 
     public static void main(String[] args) {
