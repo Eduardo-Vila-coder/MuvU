aefea0c5169e883bad39ed3986f192246273c5df
Mathias <mathias.cavalcanti@utec.edu.pe>
2026-09-23 19:51:25 -0500

    feat: Agregando paginado a habitacion

--- desarrollo/src/main/java/com/example/desarrollo/controller/HabitacionController.java
+++ desarrollo/src/main/java/com/example/desarrollo/controller/HabitacionController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -44,6 +45,15 @@ public class HabitacionController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<HabitacionResponseDTO>> getAllHabitaciones(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        // Se le agrego el Sort.by("esDestacada").descending() para que salgan las que pagan primero, sino solo se quita
+        Pageable pageable = PageRequest.of(page, size, Sort.by("esDestacada").descending());
+        return ResponseEntity.ok(habitacionService.findAll(pageable));
     }
 
     // Ruta RESTful mejorada: /habitacion/{id}/imagenes/{imagenId}
