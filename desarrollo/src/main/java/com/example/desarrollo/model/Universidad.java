de34733678a23c68011a9677499c5f4a0fa87df0
MQS144 <64620362+hfuv@users.noreply.github.com>
2026-09-24 00:45:30 -0500

    fix: preparaciones y arreglos de global

--- desarrollo/src/main/java/com/example/desarrollo/model/Universidad.java
+++ desarrollo/src/main/java/com/example/desarrollo/model/Universidad.java
@@ -1,6 +1,7 @@
 package com.example.desarrollo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +19,10 @@ public class Universidad {
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nombre;
 
+    @NotBlank
     private String direccion;
 
     @Column(nullable = true)
